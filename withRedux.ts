3073147a920faffd4914e1365aa9d53b2d850856
Ahmed Fasih <wuzzyview@gmail.com>
2019-11-19 22:22:30 -0500

    Quiz in, no history display though

--- withRedux.ts
+++ withRedux.ts
@@ -1,12 +1,12 @@
 import {AbstractIterator} from 'abstract-leveldown';
-import {Quiz, QuizGraph, textToGraph} from 'curtiz-parse-markdown';
-import {KeyToEbisu} from 'curtiz-quiz-planner';
-import {enumerate, partitionBy} from 'curtiz-utils';
+import {Quiz, QuizGraph, QuizKind, textToGraph} from 'curtiz-parse-markdown';
+import {KeyToEbisu, whichToQuiz} from 'curtiz-quiz-planner';
+import {enumerate, kata2hira, partitionBy} from 'curtiz-utils';
 import * as web from 'curtiz-web-db';
-import {Furigana, stringToFurigana} from 'jmdict-furigana-node';
+import {Furigana, furiganaToString, stringToFurigana} from 'jmdict-furigana-node';
 import leveljs from 'level-js';
 import {LevelUp} from 'levelup';
-import React, {useState} from 'react';
+import React, {useMemo, useRef, useState} from 'react';
 import ReactDOM from 'react-dom';
 import {Provider, useDispatch, useSelector} from 'react-redux';
 import {applyMiddleware, createStore, Dispatch} from 'redux';
@@ -31,7 +31,7 @@ Each synchronous action just needs a single action type.
 
 Just types, nothing else.
 */
-type Action = ReqDb|SaveDoc|LearnItem;
+type Action = ReqDb|SaveDoc|LearnItem|QuizItem;
 
 interface ReqDbBase {
   type: 'reqDb';
@@ -57,6 +57,11 @@ interface SaveDoc {
 
 interface LearnItem {
   type: 'learnItem';
+  ebisus: GraphType['ebisus'];
+}
+
+interface QuizItem {
+  type: 'quizItem';
   ebisus: GraphType['ebisus'];
 }
 
@@ -97,7 +102,7 @@ function rootReducer(state = INITIAL_STATE, action: Action): State {
     const graph: GraphType = {...emptyGraph(), ebisus: state.graph.ebisus};
     docs.forEach((doc, i) => {textToGraph(doc.content, graph)});
     return {...state, docs, graph};
-  } else if (action.type === 'learnItem') {
+  } else if (action.type === 'learnItem' || action.type === 'quizItem') {
     const graph = {...state.graph, ebisus: action.ebisus};
     return {...state, graph};
   }
@@ -148,6 +153,14 @@ function toggleLearnStatusThunk(db: Db, graph: GraphType, uids: string[]) {
   }
 }
 
+function quizItemThunk(db: Db, graph: GraphType, result: boolean, key: string, summary: string) {
+  return async (dispatch: Dispatch) => {
+    await web.updateQuiz(db, result, key, graph);
+    const action: QuizItem = {type: 'quizItem', ebisus: graph.ebisus};
+    dispatch(action);
+  }
+}
+
 /*
 # Step 5. Create the store.
 */
@@ -167,13 +180,8 @@ function EditableDoc(props: {doc: Doc, saveDoc: SaveDocType}) {
       'div',
       null,
       ce('input', {type: 'text', value: title, onChange: e => setTitle(e.target.value)}),
-      ce('textarea', {
-        value: content,
-        onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
-          // console.log('onChange', e);
-          setContent(e.target.value)
-        }
-      }),
+      ce('textarea',
+         {value: content, onChange: (e: React.ChangeEvent<HTMLInputElement>) => { setContent(e.target.value) }}),
       ce('button', {onClick: (_: any) => { props.saveDoc(props.doc, content, title, new Date()); }}, 'Submit'),
   )
 }
@@ -190,15 +198,6 @@ function Editor(props: EditorProps) {
       ce('p', null, props.docs.length + ' docs!'),
       ...props.docs.map(doc => ce(EditableDoc, {doc, saveDoc: props.saveDoc})),
   );
-}
-
-type GraphViewerProps = {
-  graph: GraphType
-};
-function GraphViewer(props: GraphViewerProps) {
-  return ce(
-      'pre', null,
-      JSON.stringify(Object.entries(props.graph).map(([key, val]) => ({[key]: Array.from(val as any)})), null, 1));
 }
 
 function FuriganaComponent(props: {furigana?: Furigana[], furiganaString?: string}) {
@@ -249,6 +248,108 @@ function ShowDocs(props: {docs: Doc[], graph: GraphType, toggleLearnStatus: (key
   return ce('ul', {}, lis);
 }
 
+function useFocus() {
+  // Via https://stackoverflow.com/a/54159564/500207
+  const ref = useRef(null);
+  const focus = () => { ref.current && (ref.current as any).focus() };
+  return {focus, ref};
+};
+function wrap(s: string) { return `_(${s})_` }
+function crossMatch(long: string[], short: string[]): boolean {
+  return long.length >= short.length ? long.some(a => short.includes(a)) : crossMatch(short, long);
+}
+const shuf: (v: any[]) => any[] = require('array-shuffle');
+function toTranslationString(quiz: Quiz) { return quiz.translation ? Object.values(quiz.translation).join(' / ') : ''; }
+function AQuiz(props: {quiz: Quiz, update: (result: boolean, key: string, summary: string) => any}) {
+  const quiz = props.quiz;
+  type GraderType = (s: string) => boolean;
+  const {grader, prompt, passive} = useMemo(() => {
+    let grader: GraderType;
+    let prompt = '';
+    let passive = false;
+
+    if (quiz.kind === QuizKind.Cloze) {
+      let promptIdx = 0;
+      prompt =
+          quiz.contexts
+              .map(context => context === null ? (quiz.prompts && wrap(quiz.prompts[promptIdx++]) || '___') : context)
+              .join('') +
+          ' ' + toTranslationString(quiz);
+      grader = (s: string) => crossMatch(quiz.clozes[0], [s, kata2hira(s)]);
+    } else if (quiz.kind === QuizKind.Card) {
+      if (quiz.subkind === 'passive') {
+        prompt = (quiz.lede ? furiganaToString(quiz.lede) : quiz.prompt);
+        if (quiz.translation) { prompt += ` (${toTranslationString(quiz)})`; }
+        grader = () => true;
+        passive = true;
+      } else {
+        prompt = quiz.prompt;
+        grader = (s: string) => crossMatch(quiz.responses.concat(quiz.prompt), [s, kata2hira(s)]);
+      }
+    } else if (quiz.kind === QuizKind.Match) {
+      const idxs: number[] = shuf(Array.from(Array(quiz.pairs.length), (_, n) => n));
+      const texts = quiz.pairs.map((o, i) => `(${i + 1})` + furiganaToString(o.text));
+      const tls = quiz.pairs.map(o => o.translation['en']);
+      const shuffledTls = idxs.map(i => tls[i] + '=?');
+      prompt = `Match ${texts.join('。 ')}。 Choices: ・${shuffledTls.join(' ・')}`;
+      grader = (s: string) => s === idxs.map(n => n + 1).join('');
+    } else {
+      throw new Error('unknown quiz type');
+    }
+    return {grader, prompt, passive};
+  }, [quiz.uniqueId]);
+
+  const [input, setInput] = useState('');
+  const {focus, ref} = useFocus();
+  return ce(
+      'div',
+      null,
+      FuriganaComponent({furiganaString: prompt}),
+      ce(
+          'form',
+          {
+            onSubmit: (e: any) => {
+              e.preventDefault();
+              const grade = grader(input);
+              const summary = (grade ? '🙆‍♂️🙆‍♀️! ' : '🙅‍♀️🙅‍♂️. ') +
+                              (passive ? '' : `「${input}」for `) + `${prompt}` +
+                              (quiz.lede ? ` ・ ${furiganaToString(quiz.lede)}` : '') + toTranslationString(quiz) +
+                              ` @ ${(new Date()).toISOString()}`;
+              props.update(grade, quiz.uniqueId, summary);
+              setInput('');
+              focus();
+            },
+          },
+          passive ? '' : ce('input', {
+            value: input,
+            type: 'text',
+            name: 'name',
+            onChange: e => setInput(e.target.value),
+            autoFocus: true,
+            ref
+          }),
+          ce('button', {disabled: passive ? false : !input, type: 'submit'}, 'Submit'),
+          ce('button', {
+            onClick: (e: any) => {
+              e.preventDefault();
+              const summary = `🤷‍♂️🤷‍♀️ ${prompt} ・ ${
+                  (quiz.lede && furiganaToString(quiz.lede) || '')} ${toTranslationString(quiz)}`;
+              props.update(false, quiz.uniqueId, summary);
+              setInput('');
+              focus();
+            }
+          },
+             'I give up'),
+          ),
+  );
+}
+
+function Learn(props: {graph: GraphType, update: (result: boolean, key: string, summary: string) => any}) {
+  const bestQuiz = whichToQuiz(props.graph);
+  if (bestQuiz) { return ce(AQuiz, {quiz: bestQuiz, update: props.update}) }
+  return ce('div', {}, 'Nothing learned to quiz!');
+}
+
 function App() {
   const {db, docs, dbLoading, graph} =
       useSelector(({db, docs, dbLoading, graph}: State) => ({db, docs, dbLoading, graph}));
@@ -259,12 +360,21 @@ function App() {
   };
 
   const editorProps: EditorProps = {docs, saveDoc};
-  const graphViewProps: GraphViewerProps = {graph};
+  const update = (result: boolean, key: string, summary: string) => {
+    if (db) { dispatch(quizItemThunk(db, graph, result, key, summary)); }
+  };
+  const learnProps = {graph, update};
   const toggleLearnStatus = (keys: string[]) => {
     if (db) { dispatch(toggleLearnStatusThunk(db, graph, keys)); }
   };
   const showDocsProps = {graph, docs, toggleLearnStatus};
-  return ce('div', null, ce(Editor, editorProps), ce(ShowDocs, showDocsProps), ce(GraphViewer, graphViewProps));
+  return ce(
+      'div',
+      null,
+      ce(Editor, editorProps),
+      ce(Learn, learnProps),
+      ce(ShowDocs, showDocsProps),
+  );
 }
 
 // Render!
